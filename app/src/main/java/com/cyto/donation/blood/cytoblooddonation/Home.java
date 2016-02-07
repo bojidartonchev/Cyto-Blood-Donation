433c5ed9ae00e3b2570335eab4be2f0900e82fee
Bozhidar Tonchev <bojidar.tonchev@gmail.com>
2016-02-07 20:01:51 +0200

    Initial commit - Home and Login screens are working

---
+++ app/src/main/java/com/cyto/donation/blood/cytoblooddonation/Home.java
@@ -0,0 +1,108 @@
+package com.cyto.donation.blood.cytoblooddonation;
+
+import android.app.Activity;
+import android.graphics.PorterDuff;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class Home extends Activity {
+    Button myProfileBtn;
+    Button bloodDonationBtn;
+    Button diaryBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        //turn title off
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_home);
+
+        this.myProfileBtn = (Button)findViewById(R.id.my_profile_btn);
+        this.bloodDonationBtn = (Button)findViewById(R.id.blood_donation_btn);
+        this.diaryBtn = (Button)findViewById(R.id.diary_btn);
+        Typeface type = Typeface.createFromAsset(getAssets(), "fonts/Grandstander.ttf");
+        this.myProfileBtn.setTypeface(type);
+        this.bloodDonationBtn.setTypeface(type);
+        this.diaryBtn.setTypeface(type);
+
+        this.myProfileBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        Button view = (Button) v;
+                        view.getBackground().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
+                        v.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP:
+                        //Intent i = new Intent(getBaseContext(), Game.class);
+                        //startActivity(i);
+
+                    case MotionEvent.ACTION_CANCEL: {
+                        Button view = (Button) v;
+                        view.getBackground().clearColorFilter();
+                        view.invalidate();
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
+
+        this.bloodDonationBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        Button view = (Button) v;
+                        view.getBackground().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
+                        v.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP:
+                        //Intent i = new Intent(getBaseContext(), Game.class);
+                        //startActivity(i);
+
+                    case MotionEvent.ACTION_CANCEL: {
+                        Button view = (Button) v;
+                        view.getBackground().clearColorFilter();
+                        view.invalidate();
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
+
+        this.diaryBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        Button view = (Button) v;
+                        view.getBackground().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
+                        v.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP:
+                        //Intent i = new Intent(getBaseContext(), Game.class);
+                        //startActivity(i);
+
+                    case MotionEvent.ACTION_CANCEL: {
+                        Button view = (Button) v;
+                        view.getBackground().clearColorFilter();
+                        view.invalidate();
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
+
+    }
+}
